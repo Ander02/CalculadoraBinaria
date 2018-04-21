ad60c1341b61087f6465956cd0c2915d986f4f7d
Vera <Vera@172.19.24.8>
2018-04-21 17:36:12 -0300

    Subtração OK, estrutura do programa alterado para funcionar com tamanho máximo de bits alocados, trocado o nome da variavel (binaryNumberWithoutSignal -> binaryNumber, (toFloat pre-fix)

--- src/binary/BinaryFloat.java
+++ src/binary/BinaryFloat.java
@@ -21,7 +21,7 @@ public class BinaryFloat extends BinaryInt{
     }
     
     public void righShift(){
-        for (int i = binaryNumber.length-2; i <= 0; i++) {
+        for (int i = binaryNumber.length-2; i < 0; i++) {
             this.binaryNumber[i+1] = this.binaryNumber[i];
         }
         this.binaryNumber[0] = false;
@@ -29,7 +29,7 @@ public class BinaryFloat extends BinaryInt{
     }
     
     public void leftShift(){
-        for (int i = binaryNumber.length-2; i <= 0; i++) {
+        for (int i = binaryNumber.length-2; i < 0; i++) {
             this.binaryNumber[i] = this.binaryNumber[i+1];
         }
         this.binaryNumber[binaryNumber.length-1] = false;
@@ -37,25 +37,52 @@ public class BinaryFloat extends BinaryInt{
     }
     
     public void rightShift(boolean[] bin){
-        for (int i = bin.length-2; i <= 0; i++) {
+        for (int i = bin.length-2; i < 0; i++) {
             bin[i+1] = bin[i];
         }
+        bin[0] = false;
     }
     
     public boolean[] toFloat(double x,int y) throws BinaryArrayException{
         int expA = 128;
         int intPart = (int) x;
         double floatPart = x-intPart;
+        boolean[] floatPartA = new boolean[y];
         
-        boolean[] intPartA = super.toBinary(intPart,y);
+        boolean[] intPartA = super.toBinary(intPart,y); //Parte inteira do binário
+        int count = 0;
         
-        for (int i = intPartA.length-1; i <= 0 ; i--) {
-            rightShift(intPartA);
-            expA++;
+        while(!intPartA[count]){ //Contador para ver quantos right shifts serão necessários aplicar para retornar o expoente.
+            count++;
+    }
+        expA += (count-intPartA.length);
+        
+        count = 0;
+        while(floatPart!=0&&count<=floatPartA.length-1){ //Parte quebrada do binário
+            floatPart *= 2;
+                if(floatPart>1){
+                    floatPartA[count] = true;
+                    floatPart -= 1;
+                }
+                else{
+                    floatPartA[count] = false;
+                }
+                count++;
         }
         
-        return null;
+        for (int i = 0; i < expA-128; i++) { //aplicando right shift na parte float para a soma
+            rightShift(floatPartA);          
+        }
         
+        BinaryInt sum1 = new BinaryInt(false,intPartA,y);
+        BinaryInt sum2 = new BinaryInt(false,floatPartA,y);
+        
+                               
+        sum1 = sum1.sum(sum2); //Soma das 2 partes no mesmo nível de expoente.
+        
+        this.exp = expA;         
+        
+        return sum1.binaryNumber;        
     }
     
     
