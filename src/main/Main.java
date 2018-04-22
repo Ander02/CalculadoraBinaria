a51fbd4022b9da2a479dac09f78a0dd84d6d7cf1
Anderson <anderson.pessoa02@outlook.com>
2018-04-22 19:32:51 -0300

    Complemento de dois estático

--- src/main/Main.java
+++ src/main/Main.java
@@ -15,7 +15,7 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws BinaryArrayException {
         /*
         System.out.println(new SumBitAux(false, false, false));
         System.out.println("0+0+0");
@@ -35,37 +35,27 @@ public class Main {
         System.out.println("1+1+1");
          */
 
-        /*for (int i = 0; i < 10; i++) {
+ /*for (int i = 0; i < 10; i++) {
             BinaryInt bin = new BinaryInt(i);
             bin.sum(new BinaryInt(1)).toInt();
         }*/
-        
-        
-            
-            
-        
         //BinaryInt test = Util.complementoDeUm(bin);
         //BinaryInt test2 = Util.complementoDeDois(bin);
-        BinaryInt bin = new BinaryInt(255,8);
-        BinaryInt sub = new BinaryInt(125,8);
-        try {
-            bin = bin.sub(sub);
-            System.out.println("Depois da subtração: "+Arrays.toString(bin.fullBynaryNumber())+" "+bin.toInt());
+        for (int i = 0; i < 10; i++) {
+            BinaryInt bin = new BinaryInt(i, 4);
+            BinaryInt one = new BinaryInt(1, 1);
+
+            BinaryInt sum = bin.sum(one);
+            System.out.println(sum + ": " + sum.toInt());
         }
-        catch(BinaryArrayException ex){
-            //System.out.println("Valor Somado: "+Arrays.toString(bin.fullBynaryNumber()));
-            System.out.println(ex.message);
-        }
-        
+
         //System.out.println("Array Original:   "+Arrays.toString(bin.fullBynaryNumber()));
         //System.out.println("Teste de Subtração :"+Arrays.toString(sub.fullBynaryNumber()));
         //System.out.println("Complemento de 1: "+Arrays.toString(test.fullBynaryNumber()));
         //System.out.println("Complemento de 2: "+Arrays.toString(test2.fullBynaryNumber()));
         //System.out.println("Resposta : "+sub.toInt());
-
         //System.out.println(soma);
         //System.out.println(soma.sum(bin3));
-        
         /*boolean[] bin;
         try {
             bin = BinaryInt.toBinary(255,8);
@@ -88,4 +78,4 @@ public class Main {
 
         PS. Não encontrei nenhum problema nos métodos com os números que eu testei no momento, o NULL pointer exception com numeros negativos se da
             pelo fato de um arranjo não ser iniciado no construtor pela falta do complemento de 2 não ter sido ajustado ainda.
-*/
+ */
