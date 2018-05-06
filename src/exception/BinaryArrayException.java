ae62cb1d4e794f52a7b341a0e17af562b3a531bc
AsrielDreemurr <mark04@hotmail.com.br>
2018-05-06 20:37:46 -0300

    Add files via upload

---
+++ src/exception/BinaryArrayException.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exception;
+
+/**
+ *
+ * @author Anderson
+ */
+public class BinaryArrayException extends Exception {
+
+    public String message;
+
+    public BinaryArrayException() {
+    }
+
+    public BinaryArrayException(String message) {
+        this.message = message;
+    }
+    
+    @Override
+    public String getMessage()
+    {
+        return this.message;
+    }
+
+}
