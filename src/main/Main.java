d9f3b8b968e0455c6142608b357d209dfb29b7e8
Anderson <anderson.pessoa02@outlook.com>
2018-05-09 02:09:38 -0300

    Float interface base

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 
+import binary.BinaryFloat;
 import binary.BinaryInt;
 import binary.IntDivisionResult;
 import exception.BinaryArrayException;
@@ -23,6 +24,9 @@ public class Main {
             BinaryInt bin1;
             BinaryInt bin2;
 
+            BinaryFloat float1;
+            BinaryFloat float2;
+
             Main.printMenu();
             int typeOption;
             do {
@@ -31,57 +35,99 @@ public class Main {
                 switch (typeOption) {
                     //Inteiros
                     case 0:
-                        int operationOption;
+                        int intOperationOption;
                         do {
                             Main.printOperationsOptions("Inteiro");
 
-                            operationOption = Main.requestInt();
+                            intOperationOption = Main.requestInt();
 
-                            switch (operationOption) {
+                            switch (intOperationOption) {
                                 case 0:
 
-                                    bin1 = Main.requestBinaryNumber("primeiro");
-                                    bin2 = Main.requestBinaryNumber("segundo");
+                                    bin1 = Main.requestBinaryIntNumber("primeiro");
+                                    bin2 = Main.requestBinaryIntNumber("segundo");
                                     BinaryInt sum = bin1.sum(bin2);
 
                                     Main.printBinaryIntResult(sum, "soma");
                                     break;
                                 case 1:
 
-                                    bin1 = Main.requestBinaryNumber("primeiro");
-                                    bin2 = Main.requestBinaryNumber("segundo");
+                                    bin1 = Main.requestBinaryIntNumber("primeiro");
+                                    bin2 = Main.requestBinaryIntNumber("segundo");
                                     BinaryInt sub = bin1.sub(bin2);
 
                                     Main.printBinaryIntResult(sub, "subtração");
                                     break;
                                 case 2:
 
-                                    bin1 = Main.requestBinaryNumber("primeiro");
-                                    bin2 = Main.requestBinaryNumber("segundo");
+                                    bin1 = Main.requestBinaryIntNumber("primeiro");
+                                    bin2 = Main.requestBinaryIntNumber("segundo");
                                     BinaryInt mult = bin1.mult(bin2);
 
                                     Main.printBinaryIntResult(mult, "multiplicação");
                                     break;
                                 case 3:
 
-                                    bin1 = Main.requestBinaryNumber("primeiro");
-                                    bin2 = Main.requestBinaryNumber("segundo");
+                                    bin1 = Main.requestBinaryIntNumber("primeiro");
+                                    bin2 = Main.requestBinaryIntNumber("segundo");
                                     IntDivisionResult divRes = bin1.div(bin2);
 
                                     Main.printBinaryIntDivResult(divRes, "divisão");
                                     break;
                             }
 
-                        } while (operationOption > 0);
+                        } while (intOperationOption >= 0);
                         break;
 
                     //Floats
                     case 1:
+                        int floatOperationOption;
+                        do {
+                            Main.printOperationsOptions("Float");
+
+                            floatOperationOption = Main.requestInt();
+
+                            switch (floatOperationOption) {
+                                case 0:
+
+                                    float1 = Main.requestBinaryFloatNumber("primeiro");
+                                    float2 = Main.requestBinaryFloatNumber("segundo");
+//                                    BinaryFloat sum = float1.sum(float2);
+
+//                                    Main.printBinaryIntResult(sum, "soma");
+                                    break;
+                                case 1:
+
+                                    bin1 = Main.requestBinaryIntNumber("primeiro");
+                                    bin2 = Main.requestBinaryIntNumber("segundo");
+                                    BinaryInt sub = bin1.sub(bin2);
+
+                                    Main.printBinaryIntResult(sub, "subtração");
+                                    break;
+                                case 2:
+
+                                    bin1 = Main.requestBinaryIntNumber("primeiro");
+                                    bin2 = Main.requestBinaryIntNumber("segundo");
+                                    BinaryInt mult = bin1.mult(bin2);
+
+                                    Main.printBinaryIntResult(mult, "multiplicação");
+                                    break;
+                                case 3:
+
+                                    bin1 = Main.requestBinaryIntNumber("primeiro");
+                                    bin2 = Main.requestBinaryIntNumber("segundo");
+                                    IntDivisionResult divRes = bin1.div(bin2);
+
+                                    Main.printBinaryIntDivResult(divRes, "divisão");
+                                    break;
+                            }
+
+                        } while (floatOperationOption >= 0);
 
                         break;
                 }
 
-            } while (typeOption > 0);
+            } while (typeOption >= 0);
 
             Main.sayGoodBye();
 
@@ -128,22 +174,53 @@ public class Main {
         } catch (Exception e) {
             return -1;
         }
-
     }
 
-    private static BinaryInt requestBinaryNumber(String order) {
-        //Scanner input = new Scanner(System.in);
+    private static float requestFloat() {
+        try {
+            Scanner input = new Scanner(System.in);
+            System.out.print(">");
+            float num = input.nextFloat();
+            System.out.println();
+            return num;
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    private static BinaryInt requestBinaryIntNumber(String order) {
         boolean ok = false;
 
         while (!ok) {
             System.out.println("Digite o " + order + " número em decimal");
-            int num1 = Main.requestInt();
+            int number = Main.requestInt();
 
             System.out.println("Digite o tamanho em bits desse número");
-            int bitSize1 = Main.requestInt();
+            int bitSize = Main.requestInt();
             try {
 
-                return new BinaryInt(num1, bitSize1);
+                return new BinaryInt(number, bitSize);
+
+            } catch (Exception e) {
+                System.out.println("Ocorreu um problema, tente novamente");
+                ok = false;
+            }
+        }
+        return null;
+    }
+
+    private static BinaryFloat requestBinaryFloatNumber(String order) {
+        boolean ok = false;
+
+        while (!ok) {
+            System.out.println("Digite o " + order + " número em decimal");
+            float number = Main.requestFloat();
+
+            System.out.println("Digite a precisão desse número");
+            int precision = Main.requestInt();
+            try {
+
+                return new BinaryFloat(number, precision);
 
             } catch (Exception e) {
                 System.out.println("Ocorreu um problema, tente novamente");
